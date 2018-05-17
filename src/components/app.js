b3da266ef94744c71450d60f54e82814fc7eaafb
Chandler Ellis <chandler.ellis47@gmail.com>
2018-05-17 14:17:25 -0600

    Added skew to clock

--- src/components/app.js
+++ src/components/app.js
@@ -9,10 +9,11 @@ export default class App extends Component {
         <h1>Birthday Countdown</h1>
           <div className="header_skew">
             <div className="header-subskew">
-                 <BirthdayForm />
+                 
               </div>
             </div>
         </header>
+        <BirthdayForm />
       </div>
     );
   }
