ceaefa67417dbe1e51920186a256b81e7b62ab33
Chandler Ellis <chandler.ellis47@gmail.com>
2018-05-17 10:53:45 -0600

    Added same day birthday function

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -37,6 +37,8 @@ class Clock extends Component {
             }
             else if (birthDay < currentDay) {
                 bday.setFullYear(today.getFullYear() + 1);
+            } else (birthDay == currentDay); {
+                return 0
             }
         }
         
@@ -80,18 +82,25 @@ class Clock extends Component {
     
     render() {
         const data = this.state.timeRemaining
-
         return (
             <div>
-                <div>
-                <div>DAYS {data.days}</div>
-                <div>HRS {data.hours}</div>
-                <div>MINS {data.minutes}</div>
-                <div>SECS {data.seconds}</div>
+            {
+                this.state.timeRemaining == 0 ?
+                     <h1>Happy Birthday</h1>
+                :
+                <div> 
+                  <div>
+                    <div>DAYS {data.days}</div>
+                    <div>HRS {data.hours}</div>
+                    <div>MINS {data.minutes}</div>
+                    <div>SECS {data.seconds}</div>
                 </div>
                 <div>
                     {<h4>remaining until you are {this.getAge()}</h4>}
-               </div>     
+                 </div>   
+            </div>
+                  
+            }
             </div>
         )
     }
