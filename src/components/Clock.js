b74bbbc93d2851ee75787164a47bd9c441371db7
Chandler Ellis <chandler.ellis47@gmail.com>
2018-05-17 11:07:55 -0600

    added no birth year message

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -8,6 +8,7 @@ class Clock extends Component {
         this.timer = 0
         this.birthday = props.birthdayFormState.startDate.toString();
         this.getTimeRemaining = this.getTimeRemaining.bind(this);
+        this.noBirthYear = new Date(this.birthday).getFullYear() == new Date().getFullYear()
 
         this.state = {
             timeRemaining: this.getTimeRemaining(props.birthdayFormState.startDate.toString())
@@ -79,6 +80,17 @@ class Clock extends Component {
         componentWillUnmount() {
             clearInterval(this.timer);
         }
+
+        renderMessage = function() {
+            if(this.noBirthYear) {
+                return (
+                    <h4>until your birthday!</h4>
+                )
+            }
+            return (
+                <h4>remaining until you are {this.getAge()}</h4>
+            )
+        }.bind(this)
     
     render() {
         const data = this.state.timeRemaining
@@ -96,7 +108,7 @@ class Clock extends Component {
                     <div>SECS {data.seconds}</div>
                 </div>
                 <div>
-                    {<h4>remaining until you are {this.getAge()}</h4>}
+                    {this.renderMessage()}
                  </div>   
             </div>
                   
